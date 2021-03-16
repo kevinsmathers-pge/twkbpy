eb7521656ff9cd1de7882fc765c630d46b8377ff
Kevin Smathers <k0sf@pge.com>
2021-03-15 21:47:22 -0700

    Added setup.py for installable library

---
+++ setup.py
@@ -0,0 +1,21 @@
+import sys
+from setuptools import setup
+
+if sys.version_info < (3, 0):
+    sys.exit('Sorry, Python lower than 3.0 is not supported')
+
+setup(
+    name = "twkbpy",
+    version = "0.1",
+    author = "DCA",
+    author_email = "kevin@ank.com",
+    description = ("TWKB GIS Loader in pure python"),
+    license = "MIT",
+    keywords = "",
+    url = "https://github.com/kevinsmathers-pge/twkbpy",
+    packages = ['twkbpy'],
+    classifiers = [],
+    install_requires = [
+
+    ]
+)
